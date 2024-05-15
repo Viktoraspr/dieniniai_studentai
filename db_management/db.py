941018bfa0250f9e3901953fd9403f959630e18b
vpranck <viktoras.pranckietis@orioninc.com>
2024-05-15 14:03:24 +0300

    Arnoldo pageidavimas

--- db_management/db.py
+++ db_management/db.py
@@ -15,12 +15,15 @@ class DB:
 
         print('Query executed')
 
+    def execute_sql_query_and_return_data(self, sql_query: str):
+        data = self.cursor.execute(sql_query)
+        return data.fetchall()
+
     def drop_table(self, table: str):
         sql_query = f"DROP TABLE IF EXISTS {table};"
         self.execute_sql_query(sql_query=sql_query)
 
     def add_values_to_transmission_table(self, **kwargs):
-        #   {'transmission': '5 gears', 'transmission_type': 'Manual',},
         transmission = kwargs.get('transmission', '5 gears')
         transmission_type = kwargs.get('transmission_type', 'Automatic')
 
@@ -29,6 +32,30 @@ class DB:
         VALUES ("{transmission}", "{transmission_type}");
         """
         self.execute_sql_query(sql_query=sql_query)
+
+    def read_data_from_table(self, table: str):
+        sql_query = f'select * from {table};'
+        return self.execute_sql_query_and_return_data(sql_query=sql_query)
+
+    def update_value_in_table(self, data: dict):
+        """UPDATE Customers
+        SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+        WHERE CustomerID = 1;
+        """
+        set_value = 'SET'
+        for key, value in data['values'].items():
+            set_value += f' {key} = "{value}",'
+        set_value = set_value[:-1]
+
+        sql_query = f"""
+        UPDATE {data['table']}
+        {set_value}
+        {data['where']};
+        """
+        self.execute_sql_query(sql_query=sql_query)
+
+    def delete_rows_from_table(self, data: dict):
+        pass
 
     def close_connection(self):
         self.connection.close()
--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from db_management.table_data import TRANSMISSION_DATA
 
 # CRUD
 
-connection = sqlite3.connect("database/mano_duomenu_baze.db")
-cursor = connection.cursor()
-
 from db_management.db import DB
 
 db = DB()
@@ -18,7 +15,32 @@ db = DB()
 # for table_query in TABLES:
 #     db.execute_sql_query(table_query)
 
-for data in TRANSMISSION_DATA:
-    db.add_values_to_transmission_table(**data)
+# for data in TRANSMISSION_DATA:
+#     db.add_values_to_transmission_table(**data)
+#
+# data = db.read_data_from_table('transmission')
+# print(data)
+#
+# result = [d for d in data if d[2] == 'Automatic']
+# print(result)
+
+data = {
+    'table': 'transmission',
+    'values': {
+        'transmission': '1 gear',
+        'transmission_type': 'auto'
+    },
+    'where': 'where transmission_id = 3'
+}
+
+db.update_value_in_table(data)
+
+
+data = {
+    'table': 'transmission',
+    'transmission_id': [1, 3, 5]
+}
+
+db.delete_rows_from_table(data)
 
 db.close_connection()
